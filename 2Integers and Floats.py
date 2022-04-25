b6f579c7a2a05757323ac40922454d950afb4ef2
Milad Zarour <Miladzarour@hotmail.com>
2022-04-25 23:02:09 +0200

    + - * / // %

--- 2Integers and Floats.py
+++ 2Integers and Floats.py
@@ -1,2 +1,19 @@
 num = 3
+
+
 print(type(num))
+
+# Arithmetic Operation
+# Addition:             3 + 2
+# Subtraction:          3 - 2
+# Multiplication:       3 * 2
+# Division:             3 / 2
+# Floor Division:       3 // 2
+# Exponent:             3 ** 2
+# Modulus:              3 % 2
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
