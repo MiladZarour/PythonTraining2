590c9a9e36b07aed62da180a8bdefd12812564e1
Milad Zarour <Miladzarour@hotmail.com>
2022-04-28 00:28:55 +0200

    not

--- 5ConditionalsandBooleansIf,Else,andElifStatements.py
+++ 5ConditionalsandBooleansIf,Else,andElifStatements.py
@@ -6,9 +6,64 @@
 # Greater or Equal  3 >= 2
 # Less or Equal:    3 <= 2
 
-language = 'Python'
+from operator import truediv
 
-if language == 'Python':
+
+language_1 = 'Python'
+language_2 = 'Java'
+language_3 = 'JavaScript'
+
+# if language == 'Pytho': this will run the else statment
+if language_1 == 'Python':
     print('Language is python')
+elif language_1 == 'Java':
+    print('Language is Java')
 else:
-    print('No match')
+    print('No Match')
+
+if language_2 == 'Python':
+    print('Language is python')
+elif language_2 == 'Java':
+    print('Language is Java')
+else:
+    print('No Match')
+
+
+if language_3 == 'Python':
+    print('Language is python')
+elif language_3 == 'Java':
+    print('Language is Java')
+elif language_3 == 'JavaScript':
+    print('Language is JavaScript')
+else:
+    print('No Match')
+print()
+
+# and
+# or
+# not
+
+user = 'Admin'
+logged_in = True
+
+if user == 'Admin' and logged_in:
+    print('Admin Page')
+else:
+    print('Bad Creds')
+
+logged_in = False
+if user == 'Admin' or logged_in:
+    print('Admin Page')
+else:
+    print('Bad Creds')
+
+if not logged_in:
+    print('Please Log In')
+else:
+    print('Welcome')
+
+logged_in = True
+if not logged_in:
+    print('Please Log In')
+else:
+    print('Welcome')
