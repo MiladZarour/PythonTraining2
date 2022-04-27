4b2dfb27dc4507b3f959d96ac469c92bc24fee32
Milad Zarour <Miladzarour@hotmail.com>
2022-04-27 23:44:25 +0200

    values()keys()items()

--- 4DictionariesWorkingwithKey-ValuePairs.py
+++ 4DictionariesWorkingwithKey-ValuePairs.py
@@ -1,3 +1,7 @@
+from multiprocessing.sharedctypes import Value
+from turtle import st
+
+
 student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
 print(student)
 print(student['name'])
@@ -14,3 +18,18 @@ student['name'] = 'Jane'
 print(student)
 student.update({'name': 'Milad', 'age': 30, 'phone': '0762602923'})
 print(student)
+del student['age']
+phone = student.pop('phone')
+print(student)
+print(phone)
+print(len(student))  # we have two keys that's why is 2
+print(student.values())
+print(student.keys())
+print(student.items())
+print()
+
+for key in student:
+    print(key)
+print()
+for key, value in student.items():
+    print(key, value)
