006521e517a2ceb4ef154163a7ba3da5d0f57694
Milad Zarour <Miladzarour@hotmail.com>
2022-04-27 23:47:19 +0200

    # Comparisons comment

--- 5ConditionalsandBooleansIf,Else,andElifStatements.py
+++ 5ConditionalsandBooleansIf,Else,andElifStatements.py
@@ -1,4 +1,14 @@
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal  3 >= 2
+# Less or Equal:    3 <= 2
+
 language = 'Python'
 
 if language == 'Python':
     print('Conditional was True')
+
+
