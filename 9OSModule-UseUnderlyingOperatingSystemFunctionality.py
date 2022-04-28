eb2cbd0678476a7d3e3e20d25334b86275d3062c
Milad Zarour <Miladzarour@hotmail.com>
2022-04-28 11:36:53 +0200

    working with path

--- 9OSModule-UseUnderlyingOperatingSystemFunctionality.py
+++ 9OSModule-UseUnderlyingOperatingSystemFunctionality.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from xmlrpc.client import DateTime
 
 size = 'The Size of the file is: '
-# print(dir(os))
+# print(dir(os)) # PRINING EVERYTHING
 print(os.getcwd())
 os.chdir('C:/Users/Milad Zarour/Desktop')
 print(os.getcwd())
@@ -28,6 +28,20 @@ print(os.listdir())
 print()
 print(os.stat('text.txt'))
 print(os.stat('text.txt').st_size)  # 36
+mod_size = str(os.stat('text.txt').st_size)
 mod_time = os.stat('text.txt').st_mtime  # st_mtime=1651137289
 print(datetime.fromtimestamp(mod_time))  # 2022-04-28 11:14:49.164686
+print(size + mod_size)  # The Size of the file is: 36
 print()
+
+# os.walk() # three values of tuples
+for dirpath, dirnames, filenames in os.walk('C:/Users/Milad Zarour/Desktop'):
+    print('Current Path:', dirpath)
+    print('Directories:', dirnames)
+    print('Files:', filenames)
+    print()
+
+print(os.environ.get('HOME'))  # C:\Users\Milad Zarour
+# file_path = os.environ.get('HOME') + 'testing.txt'
+file_path = os.path.join(os.environ.get('HOME'), 'testing2.txt')
+print(file_path)
